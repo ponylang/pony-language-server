b85df733a501b393bf4d1ed8c589ab6b83139222
Jairo <jairo@agentero.com>
2023-01-19 18:14:03 +0100

    build

--- build_ci.sh
+++ build_ci.sh
@@ -11,10 +11,14 @@ export PATH=/Users/runner/.local/share/ponyup/bin:$PATH
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
 ponyup default ubuntu22.04
 ponyup update ponyc release-$PONY_VERSION
+# copy stdlib to extension
 cd ponyc && git fetch origin && git checkout tags/$PONY_VERSION
 cd $GITHUB_WORKSPACE && cp -r ponyc/packages client_vscode
-cd $GITHUB_WORKSPACE/client_vscode
+# build pony-lsp
+cd $GITHUB_WORKSPACE
+ponyc
 # compile the extension
+cd $GITHUB_WORKSPACE/client_vscode
 npm i
 npm i -g vsce
 npm run compile
