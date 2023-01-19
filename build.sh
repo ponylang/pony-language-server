abd61ae7e348234e1f548347676021a03d44d202
Jairo <kidandcat@gmail.com>
2023-01-19 13:14:36 +0100

    up

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 # prepare environment
 PONY_VERSION=0.53.0
 set -e
-source ~/.bashrc
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
 # Use ponyup to install ponyc
-ponyup update ponyc debug-$PONY_VERSION
+ponyup update ponyc release-$PONY_VERSION
 # compile pony-lsp
 # ponyc/build/debug/ponyc -o client_vscode -b pony-lsp
 # we also need the pony stdlib, copy to the extension folder
