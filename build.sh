e62e7e1c2d1e784a0d25588b5d3f7076b3b955cf
Jairo <kidandcat@gmail.com>
2023-01-19 13:44:18 +0100

    up

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-# prepare environment
-export SHELL=/bin/bash
-export PATH=/home/runner/.local/share/ponyup/bin:$PATH
 PONY_VERSION=0.53.0
+
+# SCRIPT
 set -e
+export SHELL=/bin/bash
+# Linux
+export PATH=/home/runner/.local/share/ponyup/bin:$PATH
+# MacOS
+export PATH=/Users/runner/.local/share/ponyup/bin:$PATH
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
-# Use ponyup to install ponyc
 ponyup update ponyc release-$PONY_VERSION
-# compile pony-lsp
-# ponyc/build/debug/ponyc -o client_vscode -b pony-lsp
-# we also need the pony stdlib, copy to the extension folder
 cd ponyc && git fetch origin && git checkout tags/$PONY_VERSION
 cd $GITHUB_WORKSPACE && cp -r ponyc/packages client_vscode
 cd $GITHUB_WORKSPACE/client_vscode
