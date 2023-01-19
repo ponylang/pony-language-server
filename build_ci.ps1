36497b20c09b55ffe826b77372ca02b17d1c3b3f
Jairo <kidandcat@gmail.com>
2023-01-19 15:48:43 +0100

    remove binary

--- build_ci.ps1
+++ build_ci.ps1
@@ -1,10 +1,11 @@
 $Env:PONY_VERSION=0.53.0
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.ps1' -Outfile ponyup-init.ps1 &.\ponyup-init.ps1
+Set-Location $Env:GITHUB_WORKSPACE
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.ps1' -Outfile ponyup-init.ps1 
+.\ponyup-init.ps1
 ponyup update ponyc release-$Env:PONY_VERSION
 Set-Location ponyc
 git fetch origin
 git checkout tags/$Env:PONY_VERSION
-Set-Location $Env:GITHUB_WORKSPACE
 cp -r ponyc/packages client_vscode
 Set-Location $Env:GITHUB_WORKSPACE/client_vscode
 # compile the extension
