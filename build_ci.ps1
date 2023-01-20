21e0388246202433b52ad1d98b8ca7276fe3f922
Jairo <kidandcat@gmail.com>
2023-01-21 00:15:24 +0100

    remove zlib from windows build

--- build_ci.ps1
+++ build_ci.ps1
@@ -1,7 +1,5 @@
 $ErrorActionPreference = "Stop"
 Set-Location $Env:GITHUB_WORKSPACE
-# install zlib
-powershell -Command "(Invoke-WebRequest -Uri https://git.io/JnHTY -OutFile install_zlib.bat)"; ./install_zlib.bat; del install_zlib.bat
 # install ponyup
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.ps1' -Outfile ponyup-init.ps1 
 .\ponyup-init.ps1
