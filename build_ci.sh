8c7c65f96c2fbd21826e1cf0df58da478fc39bc7
Jairo <kidandcat@gmail.com>
2023-01-25 11:00:26 +0100

    fetch tags

--- build_ci.sh
+++ build_ci.sh
@@ -11,7 +11,7 @@ export PATH=/Users/runner/.local/share/ponyup/bin:$PATH
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
 ponyup update ponyc release-$PONY_VERSION
 # copy stdlib to extension
-cd ponyc && git fetch origin && git checkout tags/$PONY_VERSION
+cd ponyc && git fetch --all --tags && git checkout tags/$PONY_VERSION
 cd $GITHUB_WORKSPACE && cp -r ponyc/packages client_vscode
 # build pony-lsp
 cd $GITHUB_WORKSPACE
