1cff97c0778f948aad4b5e192725d52147660977
Jairo <kidandcat@gmail.com>
2023-03-07 10:43:39 +0100

    fix

--- build_ci.sh
+++ build_ci.sh
@@ -11,14 +11,14 @@ export PATH=/Users/runner/.local/share/ponyup/bin:$PATH
 sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh)"
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-cd ponyc
+cd ponyc && git fetch --all --tags
 for PONY_VERSION in $(git tag)
 do
     if [ $(version $PONY_VERSION) -ge $(version "0.54.0") ]; then
         echo "Building with ponyc version: $PONY_VERSION"
         ponyup update ponyc release-$PONY_VERSION
         # copy stdlib to extension
-        cd ponyc && git fetch --all --tags && git checkout tags/$PONY_VERSION
+        git checkout tags/$PONY_VERSION
         cd $GITHUB_WORKSPACE && cp -r ponyc/packages client_vscode
         # build pony-lsp
         cd $GITHUB_WORKSPACE
